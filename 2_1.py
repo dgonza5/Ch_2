0191ca0187c347cdec77162ac5dfbe95cd8e03d6
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-03-02 19:55:30 -0800

    began chapter_2_1

---
+++ 2_1.py
@@ -0,0 +1,33 @@
+# print(3 + 2, 3 - 2, 3 * 2)
+# print(8 / 2, 8 **2, 2*(3+4))
+
+
+# speed = 50
+# timeElasped = 14
+# distance = speed * timeElasped
+# print(distance)
+
+
+# a = 2
+# b = 3
+# print(abs(1 - (4 * b)))
+# print(int((a ** b) + .8))
+# print(round(a / b, 3))
+
+# num1 = 6
+# num1 += 1
+# num2 = 7
+# num2 -= 5
+# num3 = 8
+# num3 /= 2
+# print(num1, num2, round(num3))
+# num1 = 1
+# num1 *= 3
+# num2 = 2
+# num2 **= 3
+# print(num1, num2)
+
+totalInches = 41
+feet = totalInches // 12
+inches = totalInches % 12
+print(feet, inches)
