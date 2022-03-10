9bed2bac3a245b6262b0da2233af20bb3d3d7642
Deeno Gonzalez <deenogonzalez@swuser-129-8-235-219.swlan.csufresno.edu>
2022-03-10 15:20:32 -0800

    finishing up chapter_2_4

--- 2_4.py
+++ 2_4.py
@@ -31,31 +31,93 @@
 
 # print("Words======>", words)
 
-
 ## Calculate average of grades
-grades = []     # Create the variable grades and assign it the empty list. 
+# grades = []     # Create the variable grades and assign it the empty list. 
 
-num = float(input("Enter the first grade: "))
-grades.append(num)
+# num = float(input("Enter the first grade: "))
+# grades.append(num)
 
-num = float(input("Enter the second grade: "))
-grades.append(num)
+# num = float(input("Enter the second grade: "))
+# grades.append(num)
 
-num = float(input("Enter the third grade: "))
-grades.append(num)
+# num = float(input("Enter the third grade: "))
+# grades.append(num)
 
-num = float(input("Enter the fourth grade: "))
-grades.append(num)
+# num = float(input("Enter the fourth grade: "))
+# grades.append(num)
 
-num = float(input("Enter the fifth grade: "))
-grades.append(num)
+# num = float(input("Enter the fifth grade: "))
+# grades.append(num)
 
-min_grade = min(grades)
-grades.remove(min_grade)
+# min_grade = min(grades)
+# grades.remove(min_grade)
 
-min_grade = min(grades)
-grades.remove(min_grade)
+# min_grade = min(grades)
+# grades.remove(min_grade)
 
-average = sum(grades) / len(grades)
+# average = sum(grades) / len(grades)
 
-print("Average Grade: {0:.2f}".format(average))
+# print("Average Grade: {0:.2f}".format(average))
+
+# grades.append(1)
+# grades.append(2)
+# grades.append(4)
+# grades.append(9)
+
+# print("grades", grades)
+
+# length = len(grades) #
+# print("length", length)
+# print("sliced:", grades[2:3])
+# str1= "a,b,c"
+# parts = str1.split(",")
+
+# print("parts: ", parts)
+
+# lines = ["To", "be", "or", "not", "to", "be"]
+
+# print("lines before join: ", lines)
+
+# joined = "-".join(lines)
+
+# print("joined: ", joined)
+
+# Open up the Data.txt file in read mode
+# infile = open("Data.txt", "r")
+
+# print("infile", infile)
+# names = []
+# # Using the for loop to populate the names array with names
+# for line in infile:
+#     # print("line: ", line)
+#     # print("line after striping the right side: ", line.rstrip())
+#     names.append(line.rstrip())
+
+# print("names: ", names)
+# names_using_list_comprehension = [line.rstrip() for line in infile]
+
+# print("names_using_list_comprehension: ", names_using_list_comprehension)
+# # Close the Data.txt file to perserve memory
+# infile.close()
+
+
+# infile = open("Grades.txt", 'r')
+
+# for line in infile:
+#     print("line: ", eval(line))
+
+# infile.close()
+
+
+# infile = open("Grades.txt", 'r')
+
+# list_of_numbers = [eval(line) for line in infile]
+
+# print("list_of_numbers: ", list_of_numbers)
+
+# infile.close()
+
+
+list_of_names = ("Dionicio", "John", ["A", (1, 2, 3), "B", "C"])
+
+print("list_of_names", list_of_names[2][1][-1])
